24342d26a0e5e83aacf27789c4dbbedcdb8167db
weiju.cao <caoweiju@foxmail.com>
2019-02-28 18:30:11 +0800

    docs:学习string相关方法

--- .internal/asciiSize.js
+++ .internal/asciiSize.js
@@ -6,6 +6,9 @@
  * @returns {number} Returns the string size.
  */
 function asciiSize({ length }) {
+    // string的length的属性，通过解构赋值来获取；
+    // string中的有的方法是静态方法 只允许String来调用 不能使用string实例调用
+    // String.fromCharCode() String.fromCodePoint()
   return length
 }
 
--- .internal/asciiToArray.js
+++ .internal/asciiToArray.js
@@ -6,6 +6,8 @@
  * @returns {Array} Returns the converted array.
  */
 function asciiToArray(string) {
+    // split() 方法使用指定的分隔符字符串将一个String对象分割成字符串数组，以将字符串分隔为子字符串，以确定每个拆分的位置。 
+    // 如果空字符串("")被用作分隔符，则字符串会在每个字符之间分割。
   return string.split('')
 }
 
