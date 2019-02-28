77b96ba4d93a8db49bdacd71cdbc8322a80f8e3d
weiju.cao <caoweiju@foxmail.com>
2019-02-28 16:16:25 +0800

    docs:学习数组的reduce的实现

--- .internal/arrayReduce.js
+++ .internal/arrayReduce.js
@@ -9,14 +9,18 @@
  *  the initial value.
  * @returns {*} Returns the accumulated value.
  */
-function arrayReduce(array, iteratee, accumulator, initAccum) {
+function arrayReduce(array, iteratee, accumulator, initAccum) { 
+    // 当函数的参数多余5个的时候 将需要考虑适用对象来进行处理
+    
+    // array 待处理数组    iteratee 迭代处理函数 
   let index = -1
   const length = array == null ? 0 : array.length
 
-  if (initAccum && length) {
+  if (initAccum && length) { 
+    // 使用第一个值作为初始值 【array的reduce在没有初始值的时候就会使用第一个值作为初始值，遍历将会从第二个值开始】
     accumulator = array[++index]
   }
-  while (++index < length) {
+  while (++index < length) { // 遍历处理
     accumulator = iteratee(accumulator, array[index], index, array)
   }
   return accumulator
