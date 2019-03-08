58ef95ea90915632f33b72fcf7d8ef0823b04cfd
weiju.cao <caoweiju@foxmail.com>
2019-03-08 14:58:15 +0800

    fix:学习array的push方法的其他实现

--- .internal/baseFlatten.js
+++ .internal/baseFlatten.js
@@ -12,6 +12,18 @@ import isFlattenable from './isFlattenable.js'
  * @returns {Array} Returns the new flattened array.
  */
 function baseFlatten(array, depth, predicate, isStrict, result) {
+    // 很多时候 我们都希望给一个没有的值【null undefined】进行判断赋值
+    // 逻辑或 || 这个判断的是 ’伪假‘ 对于false之外的 【'' 0 】都会被认定为
+    /**
+     * LogicalORExpression : LogicalORExpression || LogicalANDExpression
+     *  Let lref be the result of evaluating LogicalORExpression. 【先计算逻辑或的左边表达式的值】
+     *  Let lval be GetValue(lref). 【取值的内容】值本身 基本类型 对象 函数 等
+     *  Let lbool be ToBoolean(lval). 【取布尔值】
+     *  ReturnIfAbrupt(lbool). 【左侧布尔值是真就直接返回 后面不作处理】
+     *  If lbool is true, return lval. 【返回左侧值，结束】
+     *  Let rref be the result of evaluating LogicalANDExpression. 【左侧不是真，得出左侧表达式的值】
+     *  Return GetValue(rref). 【直接返回右侧值，结束】
+     */
   predicate || (predicate = isFlattenable)
   result || (result = [])
 
@@ -28,7 +40,8 @@ function baseFlatten(array, depth, predicate, isStrict, result) {
         result.push(...value)
       }
     } else if (!isStrict) {
-      result[result.length] = value
+        // 这种写法和push有什么区别
+      result[result.length] = value 
     }
   }
   return result
